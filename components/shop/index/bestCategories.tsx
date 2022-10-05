af5cf39fca587fda09a39386fcfd09b2308bcdea
Simone Bartoli <s.bartoli@se20.qmul.ac.uk>
2022-10-05 21:48:02 +0100

    Version 1.40 ~ Text Missing

--- components/shop/index/bestCategories.tsx
+++ components/shop/index/bestCategories.tsx
@@ -17,7 +17,7 @@ const BestCategories = () => {
         <section className="flex flex-col gap-8">
             <h2 className="text-3xl text-center font-semibold leading-10 px-4">Browse Our Fantastic Products</h2>
             <div className="grid xls:grid-cols-4 sm:grid-cols-2 grid-cols-1 items-stretch">
-                <div onClick={() => redirect("mozzarella")} className="relative group cursor-pointer">
+                <div onClick={() => redirect("cheese")} className="relative group cursor-pointer">
                     <div className="transition-all homepage-image z-10 group-hover:opacity-[35%]"/>
                     <div className="transition-all p-4 group-hover:bg-black opacity-80 z-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                         <span className="text-5xl text-white font-navbar">Cheese</span>
@@ -44,7 +44,7 @@ const BestCategories = () => {
                         <Image alt="image" placeholder={"blur"} src={HamImage} className={"image !max-h-[400px] xls:!max-h-full"} layout={"fill"} objectFit={"cover"} />
                     </div>
                 </div>
-                <div onClick={() => redirect("bread")} className="relative group cursor-pointer">
+                <div onClick={() => redirect("wine")} className="relative group cursor-pointer">
                     <div className="transition-all homepage-image z-10 group-hover:opacity-[35%]"/>
                     <div className="transition-all p-4 group-hover:bg-black opacity-80 z-20 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                         <span className="text-5xl text-white font-navbar">Wine</span>
